15a979a17ab745a2f75173db1c2ebab20d322ee7
yumdmb <elvissawing.muran@gmail.com>
2024-10-26 22:14:48 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import Footer from '@/components/Footer';
+import MainComponent from '@/components/MainComponent';
+import Head from 'next/head';
+import Script from 'next/script';
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Untuk: Maya❤</title>
+
+                <link
+                    href="https://fonts.googleapis.com/css?family=Work+Sans:300,400"
+                    rel="stylesheet"
+                />
+                <link rel="icon" href="favicon.ico" />
+            </Head>
+
+            <Script
+                src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"
+                strategy="lazyOnload"
+            />
+            <Script src="main.js" strategy="lazyOnload" />
+
+            <MainComponent />
+            <Footer />
+        </>
+    );
+}
